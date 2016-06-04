12a13e1c3a145e5ca0c226ab8a78fcbd675b9a64
Vladimir Radinovic <vladimir@radinovic.rs>
2016-06-05 00:24:12 +0200

    Add angular2 setup

---
+++ app/components/main_page.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'main-page',
+    templateUrl: 'app/components/main_page.component.html',
+    directives: [],
+    providers: []
+})
+export class MainPageComponent {
+}
---
+++ app/main.ts
@@ -0,0 +1,8 @@
+import {bootstrap}    from 'angular2/platform/browser';
+import {AppComponent} from './app.component';
+import {MainPageComponent} from './components/main_page.component';
+import {ShapeSelectorComponent} from "./components/shape_selector_component";
+import {NavigationBarComponent} from "./components/navigation_bar_component";
+
+// bootstrap(AppComponent);
+bootstrap(MainPageComponent);
