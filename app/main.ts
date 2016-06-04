83cb620e9debfdf9abf35eb157b3a32f0eff876c
Vladimir Radinovic <vladimir@radinovic.rs>
2016-06-05 00:28:06 +0200

    Remove unused code

--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,4 @@
 import {bootstrap}    from 'angular2/platform/browser';
-import {AppComponent} from './app.component';
 import {MainPageComponent} from './components/main_page.component';
-import {ShapeSelectorComponent} from "./components/shape_selector_component";
-import {NavigationBarComponent} from "./components/navigation_bar_component";
 
-// bootstrap(AppComponent);
 bootstrap(MainPageComponent);
